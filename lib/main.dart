83cb4272603ba9e4a36bebd2c937ae5a8b506af2
ahmed <azaeem1@smail.ucas.edu.ps>
2023-06-21 00:25:37 +0300

    Added responsiveness and edited App's Locales

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localization.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        body: Center(),
-      ),
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      builder: (BuildContext context, Widget? child) {
+        return const MaterialApp(
+          title: 'LearnEngo',
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: [Locale('en'), Locale('ar')],
+          locale: Locale('en'),
+          home: Scaffold(
+            body: Center(),
+          ),
+        );
+      },
     );
   }
 }
