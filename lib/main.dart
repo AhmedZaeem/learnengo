1c91bf34470b02ce6e69992857195e6ebc011501
ahmed <azaeem1@smail.ucas.edu.ps>
2023-06-21 00:52:05 +0300

    Initial Splash Screen fiels

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localization.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import 'Screens/splashScreen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -10,15 +12,14 @@ class MyApp extends StatelessWidget {
     return ScreenUtilInit(
       designSize: const Size(375, 812),
       builder: (BuildContext context, Widget? child) {
-        return const MaterialApp(
-          title: 'LearnEngo',
-          localizationsDelegates: AppLocalizations.localizationsDelegates,
-          supportedLocales: [Locale('en'), Locale('ar')],
-          locale: Locale('en'),
-          home: Scaffold(
-            body: Center(),
-          ),
-        );
+        return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(primaryColor: const Color(0xff3A6AE2)),
+            title: 'LearnEngo',
+            localizationsDelegates: AppLocalizations.localizationsDelegates,
+            supportedLocales: const [Locale('en'), Locale('ar')],
+            locale: const Locale('en'),
+            home: const splashScreen());
       },
     );
   }
